bb744b3270d6bbbc8f2674a2007256ee93443bcc
runnedrunner <auszhou@nuevaschool.org>
2016-11-15 09:10:09 -0800

    Classadoo autocommit

---
+++ -KVzd3GnwUs9vZ5igaMm/script.js
@@ -0,0 +1,42 @@
+// demo function
+function logArgs(args) {
+  console.log(args)
+  // Get 
+}
+
+function coolfunction(args) {
+  return args;
+}
+
+$("#button" ).click(function() {
+  console.log("hi!");
+});
+var functionString = coolfunction.toString();
+console.log(functionString)
+var consoleLogExample = $("#cool-function-example")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(functionString);
+
+consoleLogExample.html(formattedExample);
+
+var functionString = logArgs.toString();
+console.log(functionString)
+var consoleLogExample = $("#console-log-example")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(functionString);
+
+consoleLogExample.html(formattedExample);
+
+var functionString = "thing";
+console.log(functionString)
+var consoleLogExample = $("#jquery-example")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(functionString);
+
+consoleLogExample.html(formattedExample);
